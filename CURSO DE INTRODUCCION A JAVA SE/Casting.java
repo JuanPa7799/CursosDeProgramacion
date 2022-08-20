242bb3cd8ebbe51e1f822bb171a773be8fa13503
juanpa7799 <garsa779999@gmail.com>
2022-08-19 21:49:06 -0500

    Ultimo commit del dia 19 de agoso del 2022

---
+++ CURSO DE INTRODUCCION A JAVA SE/Casting.java
@@ -0,0 +1,32 @@
+public class Casting {
+    public static void main(String[] args) {
+        
+        //En un año  ubicar 30 perritos 
+        //Cuantos perritos ubique al mes?
+
+        double monthlyDogs = 30.0 /12.0;
+        System.out.println(monthlyDogs);
+
+        //Estimacion
+
+        int estimatedMonthlyDogs = (int)monthlyDogs;
+        System.out.println(estimatedMonthlyDogs);
+
+        //exactitud
+        int a = 30;
+        int b = 12;
+        System.out.println((double)a/b);
+
+        double c = (double)a/b;
+        System.out.println(c);
+
+        char n = '1';
+        int nT = n;
+        System.out.println(nT);
+
+        short nS = (short)n;
+        System.out.println(nS);
+        
+
+    }
+}
