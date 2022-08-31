9bb53556723f901603b0cdc785b1fad420595a79
juanpa7799 <garsa779999@gmail.com>
2022-08-31 17:12:28 -0500

    ultimo de la tarde del 31 de agosto de 2022 antes del cumple de Yuri

--- CURSO DE JAVA SE ORIENTADA A OBJETOS/Doctor.java
+++ CURSO DE JAVA SE ORIENTADA A OBJETOS/Doctor.java
@@ -1,19 +1,81 @@
+import java.util.ArrayList;
+
+import javax.print.DocFlavor.STRING;
+
 public class Doctor {
     //Atributos
-    int id;
+    static int id = 0; //autoincrementado
     String name; 
+    String email;
     String speciality;
+
+    
 
     Doctor(){
         System.out.println("Contruyendo el objeto Doctor");
     }
 
-    Doctor(String name){
+    Doctor(String name, String speciality){
         System.out.println("El nombre del Doctor asignado es: " + name);
+        this.name = name;
+        this.speciality = speciality;
+        id++;
     }
 
     //Comportamientos
     public void showName(){
         System.out.println(name);
     }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
+    }
+
+    ArrayList<AvailableAppontment> availableappontments = new ArrayList<>();
+    public void addAvailableappontment(String date, String time) {
+        availableappontments.add(new Doctor.AvailableAppontment(date, time));
+    }
+
+    public ArrayList<AvailableAppontment> getAvailableAppontments(){
+        return availableappontments;
+    }
+
+    public static class AvailableAppontment{
+        private int id;
+        private String date;
+        private String time;
+        //Available Appointment
+        public  AvailableAppontment(String date, String time) {
+            this.date=date;
+            this.time=time;
+            
+        }
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+        
+
+    }
+    
 }
+
--- CURSO DE JAVA SE ORIENTADA A OBJETOS/Main.java
+++ CURSO DE JAVA SE ORIENTADA A OBJETOS/Main.java
@@ -1,8 +1,56 @@
+import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("anahi Salgado");
-        myDoctor.name="Alejandro Rodriguez";
-        myDoctor.showName();
+
+        //showMenu();
+
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
+        myDoctor.addAvailableappontment("hoy", "4pm");
+        myDoctor.addAvailableappontment("hoy", "10am");
+        myDoctor.addAvailableappontment("hoy", "1pm");
+
+        for (Doctor.AvailableAppontment Aa : myDoctor.getAvailableAppontments()) {
+            System.out.println(Aa.getDate()+" " + Aa.getTime());
+            
+        }
+
+        System.out.println(myDoctor.getAvailableAppontments());
+
+
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
+
+        int i = 0;
+        int b = 2;
+        b = i;
+        //b=0
+        String name = "Ann";
+
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        Patient patient2 = new Patient("Anahi", "anahi@mail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        /*patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());*/
+
     }
+
+
+
 }
